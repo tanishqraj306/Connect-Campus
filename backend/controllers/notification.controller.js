58ac1cbc83453ea0a4ee0f2a77e8acb6e53455e4
Tanishq Raj <tanishqraj306@gmail.com>
2024-11-18 16:52:18 +0530

    Implemented Navbar

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,3 +1,5 @@
+import Notification from "../models/notification.model.js";
+
 export const getUserNotifications = async (req, res) => {
   try {
     const notifications = await Notification.find({
@@ -7,7 +9,7 @@ export const getUserNotifications = async (req, res) => {
       .populate("realtedUser", "name username profilePicture")
       .populate("relatedPost", "content image");
 
-    res.status(200).json({ notifications });
+    res.status(200).json(notifications);
   } catch (error) {
     console.log(`Error in getUserNotifications: ${error.message}`);
     res.status(500).json({ message: "Internal server error" });
@@ -23,8 +25,9 @@ export const markNotificationAsRead = async (req, res) => {
       { new: true },
     );
 
-    res.json({ message: "Notification deleted successfully" });
+    res.json(notification);
   } catch (error) {
+    console.log(`Error in markNotificationAsRead: ${error.message}`);
     res.status(500).json({ message: "Internal server error" });
   }
 };
